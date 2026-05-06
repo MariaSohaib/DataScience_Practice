2c32ad9060251c1e764a2f30a27bbb727e23f216
MariaSohaib <mariasohaib828@gmail.com>
2026-05-06 10:11:51 -0700

    Adding Activation Function

--- WeightCalculationFOR_NeuralNetwoks.py
+++ WeightCalculationFOR_NeuralNetwoks.py
@@ -1,3 +1,4 @@
+import math
 def Weight(x1,x2,x3):
     w1=0.9
     w2=0.5
@@ -5,8 +6,13 @@ def Weight(x1,x2,x3):
     bias=0.75
     calc=(x1*w1)+(x2*w2)+(x3*w3)+bias
     return calc
+def Activation_Function(x):
+    Sol=1/(1+math.exp(-x))
+    return Sol
 def main():
     solution=Weight(0.5,0.2,0.3)
     print(f"Solution: {solution}")
+    Solution1=Activation_Function(solution)
+    print(f"Value of Activation Function: {Solution1}")
 if __name__=="__main__":
     main()
