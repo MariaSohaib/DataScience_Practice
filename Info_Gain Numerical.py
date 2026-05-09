9283e7447b69c0606bbc3fd30ae910f1193277fd
MariaSohaib <mariasohaib828@gmail.com>
2026-05-09 06:30:44 -0700

    Info_Gain_Assignment_Column

--- Info_Gain Numerical.py
+++ Info_Gain Numerical.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import math as m
 def Info_GAIN(entropy_before,entropy_after):
     INFO_GAIN_RESULT=entropy_before-entropy_after
@@ -16,27 +17,29 @@ def probabilities(p,q):
         t2=0
     sol=-t1-t2
     return sol
-import pandas as pd
-df=pd.read_csv('CSV Files\StudentsResult.csv')
-length=len(df)
-counts=df['Assignment'].value_counts()
-prob_of_yes=float(counts[1]/length)
-prob_of_no=float(counts[0]/length)
-count_pass_yes=count_pass_no=count_fail_yes=count_fail_no=0
-for i,row in df.iterrows():
-    if row['Assignment']==0:
-        if row['Result']=='Pass':
-            count_pass_no+=1
-        else:
-            count_fail_no+=1
-    elif row['Assignment']==1:
-        if row['Result']=='Pass':
-            count_fail_yes+=1
-        else:
-            count_pass_yes+=1
-group_yes=probabilities(count_fail_yes,count_pass_yes)
-group_no=probabilities(count_fail_no,count_pass_no)
-E_before=probabilities(prob_of_yes,prob_of_no)
-E_after=(prob_of_yes*group_yes)+(prob_of_no*group_no)
-Answer=E_before-E_after
-print(Answer)
+def main():
+    df=pd.read_csv('CSV Files\StudentsResult.csv')
+    length=len(df)
+    counts=df['Assignment'].value_counts()
+    prob_of_yes=float(counts[1]/length)
+    prob_of_no=float(counts[0]/length)
+    count_pass_yes=count_pass_no=count_fail_yes=count_fail_no=0
+    for i,row in df.iterrows():
+        if row['Assignment']==0:
+            if row['Result']=='Pass':
+                count_pass_no+=1
+            else:
+                count_fail_no+=1
+        elif row['Assignment']==1:
+            if row['Result']=='Pass':
+                count_fail_yes+=1
+            else:
+                count_pass_yes+=1
+    group_yes=probabilities(count_fail_yes,count_pass_yes)
+    group_no=probabilities(count_fail_no,count_pass_no)
+    E_before=probabilities(prob_of_yes,prob_of_no)
+    E_after=(prob_of_yes*group_yes)+(prob_of_no*group_no)
+    Answer=E_before-E_after
+    print(Answer)
+if __name__=="__main__":
+    main()
